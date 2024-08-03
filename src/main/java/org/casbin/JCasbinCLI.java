178dbd2999203bf8d24fde34398e1ddb578e2930
liewstar <2935437378@qq.com>
2024-08-03 16:06:03 +0800

    feat: add removePolicy and addPolicy

---
+++ src/main/java/org/casbin/JCasbinCLI.java
@@ -0,0 +1,74 @@
+package org.casbin;
+
+import org.apache.commons.cli.*;
+import org.casbin.jcasbin.exception.CasbinConfigException;
+import org.casbin.jcasbin.main.Enforcer;
+
+public class JCasbinCLI {
+    private static void configureOptions(Options options) {
+        Option[] cliOptions = {
+                addOption("m", "model", true, "the path of the model file"),
+                addOption("p", "policy", true, "the path of the policy file"),
+                addOption("e", "enforce", true, "enforce"),
+                addOption("ex", "enforceEx", true, "enforceEx"),
+                addOption("ap", "addPolicy", true, "Add a policy rule to the storage"),
+                addOption("rp", "removePolicy", true, "Remove a policy rule from the storage")
+        };
+        for (Option option : cliOptions) {
+            options.addOption(option);
+        }
+    }
+    private static Option addOption(String shortOpt, String longOpt, boolean hasArg, String description) {
+        return new Option(shortOpt, longOpt, hasArg, description);
+    }
+
+    public static Object run(String[] args) throws ParseException {
+        Options options = new Options();
+        configureOptions(options);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        String modelPath = cmd.getOptionValue("model");
+        String policyPath = cmd.getOptionValue("policy");
+        Enforcer enforcer = null;
+        try {
+            enforcer = new Enforcer(modelPath, policyPath);
+        } catch (CasbinConfigException ex) {
+            ex.printStackTrace();
+        }
+
+        if(cmd.hasOption("enforce")) {
+            String enforceArgs = cmd.getOptionValue("enforce").replace(" ","");
+            boolean result = enforcer.enforce(enforceArgs.split(","));
+            System.out.println(result ? "Allow" : "Ban");
+            return result;
+        } else if (cmd.hasOption("enforceEx")) {
+            String enforceArgs = cmd.getOptionValue("enforceEx").replace(" ","");
+            boolean result = enforcer.enforceEx(enforceArgs.split(",")).isAllow();
+            System.out.println(result ? "Allow" : "Ban");
+            return result;
+        }else if (cmd.hasOption("addPolicy")){
+            String policyArgs = cmd.getOptionValue("addPolicy").replace(" ","");
+            boolean result = enforcer.addPolicy(policyArgs.split(","));
+            System.out.println(result ? "Add Success" : "Add Failed");
+            enforcer.savePolicy();
+            return result;
+        }else if (cmd.hasOption("removePolicy")){
+            String policyArgs = cmd.getOptionValue("removePolicy").replace(" ","");
+            boolean result = enforcer.removePolicy(policyArgs.split(","));
+            System.out.println(result ? "Remove Success" : "Remove Failed");
+            enforcer.savePolicy();
+            return result;
+        }else {
+            System.out.println("Command Error");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws ParseException {
+        JCasbinCLI cli = new JCasbinCLI();
+        Object run = cli.run(args);
+        System.out.println(run);
+    }
+}
--- src/test/java/org/casbin/ClientTest.java
+++ src/test/java/org/casbin/ClientTest.java
@@ -1,6 +1,7 @@
 package org.casbin;
 
 import org.apache.commons.cli.ParseException;
+import org.casbin.jcasbin.main.Enforcer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,26 +10,49 @@ public class ClientTest {
 
     @Test
     public void testRBAC() throws ParseException {
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data1,read"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data1,write"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data2,read"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data2,write"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data1,read"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data1,write"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data2,read"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data2,write"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data1,read"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data1,write"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data2,read"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","alice,data2,write"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data1,read"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data1,write"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data2,read"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/rbac_model.conf","-p","examples/rbac_policy.csv","-e","bob,data2,write"}), true);
     }
 
     @Test
     public void testABAC() throws ParseException {
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain1,data1,read"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain1,data1,write"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain2,data1,read"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain2,data1,write"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain1,data2,read"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain1,data2,write"}), false);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain2,data2,read"}), true);
-        assertEquals(Client.clientEnforce(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain2,data2,read"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain1,data1,read"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain1,data1,write"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain2,data1,read"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","alice,domain2,data1,write"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain1,data2,read"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain1,data2,write"}), false);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain2,data2,read"}), true);
+        assertEquals(JCasbinCLI.run(new String[]{"-m","examples/abac_rule_with_domains_model.conf","-p","examples/abac_rule_with_domains_policy.csv","-e","bob,domain2,data2,read"}), true);
+    }
+
+    @Test
+    public void testAddPolicy() throws ParseException {
+        String modelPath = "examples/rbac_model.conf";
+        String policyPath = "examples/rbac_policy.csv";
+        Enforcer enforcer = new Enforcer(modelPath, policyPath);
+        String policy = "aliceTest,data,read";
+        assertEquals(JCasbinCLI.run(new String[]{"-m",modelPath,"-p",policyPath,"-ap",policy}), true);
+        enforcer.removePolicy(policy.split(","));
+        enforcer.savePolicy();
+    }
+
+    @Test
+    public void testRemovePolicy() throws ParseException {
+        String modelPath = "examples/rbac_model.conf";
+        String policyPath = "examples/rbac_policy.csv";
+        Enforcer enforcer = new Enforcer(modelPath, policyPath);
+        String policy = "alice,data1,read";
+        assertEquals(JCasbinCLI.run(new String[]{"-m",modelPath,"-p",policyPath,"-rp",policy}), true);
+        enforcer.addPolicy(policy.split(","));
+        enforcer.savePolicy();
+
     }
 
 }
