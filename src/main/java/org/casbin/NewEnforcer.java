ea2c3de4ff37038c92d31314f8cefc80c758f184
liewstar <2935437378@qq.com>
2024-08-08 01:41:10 +0800

    feat: simple check model

--- src/main/java/org/casbin/NewEnforcer.java
+++ src/main/java/org/casbin/NewEnforcer.java
@@ -6,6 +6,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NewEnforcer extends Enforcer {
 
@@ -15,6 +22,17 @@ public class NewEnforcer extends Enforcer {
 
     public static String parse(String string, String suffix) {
         boolean isFile = string.endsWith(suffix);
+        if(suffix.equals(".conf")) {
+            if(isFile) {
+                try {
+                    simpleCheck(new String(Files.readAllBytes(Paths.get(string)), StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                simpleCheck(string);
+            }
+        }
         return isFile ? string : writeToTempFile(string, suffix);
     }
 
@@ -25,12 +43,28 @@ public class NewEnforcer extends Enforcer {
             tempFile.deleteOnExit();
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
                 writer.write(str);
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return tempFile.getAbsolutePath();
+    }
+
+    private static void simpleCheck(String fileString) {
+        fileString = fileString.replace(" ","");
+        String[] requiredSubstrings = {"[request_definition]", "[policy_definition]", "[policy_effect]", "[matchers]", "r=", "p=", "e=", "m="};
+        List<String> missingSubstrings = new ArrayList<>();
+
+        for (String substring : requiredSubstrings) {
+            Pattern pattern = Pattern.compile(Pattern.quote(substring));
+            Matcher matcher = pattern.matcher(fileString);
+            if (!matcher.find()) {
+                missingSubstrings.add(substring);
+            }
+        }
+
+        if(!missingSubstrings.isEmpty()) {
+            throw new RuntimeException("missing required sections: " + String.join(", ", missingSubstrings));
+        }
     }
 }
