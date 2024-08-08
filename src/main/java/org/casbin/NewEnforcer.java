8d9c1b329a5bd659f2926e4dbce9b052b4cd9bbc
Liao Xin <93535922+liewstar@users.noreply.github.com>
2024-08-08 21:33:05 +0800

    feat: support some functions of the editor (#9)
    * feat: Support parsing string
    
    * feat: simple check model
    
    * feat: update README

---
+++ src/main/java/org/casbin/NewEnforcer.java
@@ -0,0 +1,71 @@
+package org.casbin;
+
+import org.casbin.jcasbin.main.Enforcer;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NewEnforcer extends Enforcer {
+
+    public NewEnforcer(String modelPath, String policyFile) {
+        super(parse(modelPath, ".conf"), parse(policyFile, ".csv"));
+    }
+
+    public static String parse(String string, String suffix) {
+        string = string.replace("|","\n");
+        boolean isFile = string.endsWith(suffix);
+        if(suffix.equals(".conf")) {
+            if(isFile) {
+                try {
+                    simpleCheck(new String(Files.readAllBytes(Paths.get(string)), StandardCharsets.UTF_8));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                simpleCheck(string);
+            }
+        }
+        return isFile ? string : writeToTempFile(string, suffix);
+    }
+
+    public static String writeToTempFile(String str, String suffix) {
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("default", suffix);
+            tempFile.deleteOnExit();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+                writer.write(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempFile.getAbsolutePath();
+    }
+
+    private static void simpleCheck(String fileString) {
+        fileString = fileString.replace(" ","");
+        String[] requiredSubstrings = {"[request_definition]", "[policy_definition]", "[policy_effect]", "[matchers]", "r=", "p=", "e=", "m="};
+        List<String> missingSubstrings = new ArrayList<>();
+
+        for (String substring : requiredSubstrings) {
+            Pattern pattern = Pattern.compile(Pattern.quote(substring));
+            Matcher matcher = pattern.matcher(fileString);
+            if (!matcher.find()) {
+                missingSubstrings.add(substring);
+            }
+        }
+
+        if(!missingSubstrings.isEmpty()) {
+            throw new RuntimeException("missing required sections: " + String.join(", ", missingSubstrings));
+        }
+    }
+}
--- src/test/java/org/casbin/ClientTest.java
+++ src/test/java/org/casbin/ClientTest.java
@@ -5,6 +5,7 @@ import org.casbin.jcasbin.main.Enforcer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ClientTest {
 
@@ -52,6 +53,31 @@ public class ClientTest {
         assertEquals(Client.run(new String[]{"-m",modelPath,"-p",policyPath,"-rp",policy}), true);
         enforcer.addPolicy(policy.split(","));
         enforcer.savePolicy();
+    }
+
+    @Test
+    public void testParseString() {
+        String model = "[request_definition]\n" +
+                "r = sub, obj, act\n" +
+                "\n" +
+                "[policy_definition]\n" +
+                "p = sub, obj, act\n" +
+                "\n" +
+                "[role_definition]\n" +
+                "g = _, _\n" +
+                "\n" +
+                "[policy_effect]\n" +
+                "e = some(where (p.eft == allow))\n" +
+                "\n" +
+                "[matchers]\n" +
+                "m = g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act";
+        String policy = "p, alice, data1, read\n" +
+                "p, bob, data2, write\n" +
+                "p, data2_admin, data2, read\n" +
+                "p, data2_admin, data2, write\n" +
+                "g, alice, data2_admin";
+        NewEnforcer enforce = new NewEnforcer(model, policy);
+        assertTrue(enforce.enforce("alice", "data1", "read"));
     }
 
 }
